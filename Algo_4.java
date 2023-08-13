a5f020979d926cd7236aacee4034b036e545acb5
chamal995 <chamal995@gmail.com>
2023-08-13 23:42:16 +0530

    Algo-4 Completed

---
+++ Algo_4.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Algo_4 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50}; 
+
+        for (int i = 0; i < nums.length-3; i++) {
+            int temp;
+            temp=nums[i];
+            nums[i]=nums[nums.length-1-i];
+            nums[nums.length-1-i]=temp;
+            
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
