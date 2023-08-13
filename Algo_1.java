999cb602ea9ed7765a92077a00e508cf458eb3e9
chamal995 <chamal995@gmail.com>
2023-08-13 22:50:57 +0530

    Algo-1 Completed

--- Algo_1.java
+++ Algo_1.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Algo_1 {
+
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+
+        int num;
         
+        loop1 :
+        do {
+            System.out.print("\n Input a positive number ;");
+            num = sc.nextInt();
+            sc.nextLine();
+
+            if(num<=0) {
+                System.out.println( "invalid input");
+                continue loop1;
+            }
+
+            System.out.print("Fibonacci sequence :");
+
+            int first = 0, second = 1;
+            while (first <= num) {
+                System.out.print(first + " ");
+                int next = first + second;
+                first = second;
+                second = next;}
+         
+        } while (true);
     }
 }
