c2213fdaf76fe5a47e599f1fb8c7a5b61ae6df89
chamal995 <chamal995@gmail.com>
2023-08-14 01:21:00 +0530

    Algo-6 completed

---
+++ Algo_6.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Algo_6 {
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        String sentence;
+        
+        loop1:
+        do {
+            
+            System.out.print("Enter enter a sentence (ASCII only) :");
+            sentence = sc.nextLine();
+            
+            if(sentence.isBlank()){
+                System.out.println("Invalid input");
+                continue loop1;
+            }
+            break loop1;
+        } while (true);
+
+        String[] words = sentence.split("\\s+");
+        StringBuilder reversed = new StringBuilder();
+            for (int i = words.length - 1; i >= 0; i--) {
+                reversed.append(words[i]);
+                if (i > 0) {
+                    reversed.append(" ");
+                }
+            }
+        System.out.println(reversed);
+        
+    }
+}
